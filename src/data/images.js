cff26e3f61f05e405d7d60cffa4b3e3dc5792980
yael.lotan <yaelotan19@gmail.com>
2020-06-14 16:54:04 +0300

    fix(views/Home/components/Images.js): export images to data

---
+++ src/data/images.js
@@ -0,0 +1,23 @@
+import tuli1 from '../assets/images/tuli1.jpg';
+import tuli2 from '../assets/images/tuli2.jpg';
+import tuli3 from '../assets/images/tuli3.jpg';
+
+const homeCarouselImages = [
+    {
+        id: 1,
+        src: tuli1,
+        alt: ""
+    },
+    {
+        id: 2,
+        src: tuli2,
+        alt: ""
+    },
+    {
+        id: 3,
+        src: tuli3,
+        alt: ""
+    }
+];
+
+export const getHomeCarouselImages = () => homeCarouselImages;
