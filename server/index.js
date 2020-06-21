afa5694d21e73899ad33dd20e69c877f7d57bb1b
yael.lotan <yaelotan19@gmail.com>
2020-06-21 18:46:33 +0300

    fix(translation.json): change the content

--- server/index.js
+++ server/index.js
@@ -15,20 +15,18 @@ app.post('/send', (req, res) => {
   console.log(req.body);
   try {
     const mailOptions = {
-        from: process.env.email,
-        to: process.env.email,
-        subject: req.body.fullName, 
-        html: req.body.content
+        from: req.body.email,
+        to: process.env.EMAIL,
+        subject: 'Tulis Event - New Message', 
+        html: `<div><h4>Sender Information:</h4><p><b>Name:</b> ${req.body.name}</p><p><b>Email:</b> ${req.body.email}</p><p><b>Phone Number:</b> ${req.body.tel}</p><hr></hr><br></br><p><b>Message:</b><br></br><br></br>${req.body.content}</p></div>`
     };
 
     console.log('sending email');
     transporter.sendMail(mailOptions, function(error, info) {
-        console.log(info);
-        console.log(error);
         if (error) {
             res.status(500).send('has error');
         } else {
-            res.status(200).send('ok');
+            res.status(200).send(`<div><h4>Sender Information:</h4><p><b>Name:</b> ${req.body.name}</p><p><b>Email:</b> ${req.body.email}</p><p><b>Phone Number:</b> ${req.body.tel}</p><hr></hr><br></br><p><b>Message:</b><br></br><br></br>${req.body.content}</p></div>`);
         }
     })
   } catch (error) {
